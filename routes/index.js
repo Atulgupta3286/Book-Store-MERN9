f5b02287968b20798c35e31e90c1573afcfc1af4
Atul Gupta <princegupta9302@gmail.com>
2024-04-02 01:24:46 +0530

    first commit - 1 april

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/readall', function(req, res, next) {
+  res.render('library');
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+module.exports = router;
