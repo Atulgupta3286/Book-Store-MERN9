706c329fe9166526d07128d8082f9a243795f148
Atul Gupta <princegupta9302@gmail.com>
2024-04-03 20:12:20 +0530

    third commit - 3 april

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,16 @@ router.get('/create', function(req, res, next) {
 router.post('/create', function(req, res, next) {
   BOOK.push(req.body)
   // res.json(BOOK);
-  res.redirect('/readall')
+  res.redirect('/readall');
+});
+
+router.get('/delete/:index', function(req, res, next) {
+  BOOK.splice(req.params.index,1);
+  res.redirect('/readall');
+});
+
+router.get('/update/:id', function(req, res, next) {
+  res.send('updated');
 });
 
 router.get('/about', function(req, res, next) {
