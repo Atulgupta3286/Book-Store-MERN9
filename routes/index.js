0d8a71cce81b3e08766499a83ef4f19cfd21007d
Atul Gupta <princegupta9302@gmail.com>
2024-04-03 19:40:05 +0530

    second commit - 2 april

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const BOOK = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
 router.get('/readall', function(req, res, next) {
-  res.render('library');
+  res.render('library' , {
+    books : BOOK 
+  });
 });
 
 router.get('/create', function(req, res, next) {
   res.render('create');
+});
+
+router.post('/create', function(req, res, next) {
+  BOOK.push(req.body)
+  // res.json(BOOK);
+  res.redirect('/readall')
 });
 
 router.get('/about', function(req, res, next) {
